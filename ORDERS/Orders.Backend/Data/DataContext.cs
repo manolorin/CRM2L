2a0295dbe87e950a325b343b29966cf1ae9b8467
francisco Manuel Rincon Vargas <fmrinconv@gmail.com>
2026-09-05 17:32:29 -0600

    Se crea la Base de datos con la tabla Country

---
+++ ORDERS/Orders.Backend/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Orders.Shared.Entities;
+
+namespace Orders.Backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
---
+++ ORDERS/Orders.Shared/Entities/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Orders.Shared.Entities;
+
+public class Country
+{
+    public int Id { get; set; }
+
+    [Display(Name = "Pais")]
+    [MaxLength(100, ErrorMessage = "{0} no puede exceder los {1} caracteres.")]
+    [Required(ErrorMessage = "El nombre de {0} es requerido.")]
+    public string Name { get; set; } = null!;
+}
