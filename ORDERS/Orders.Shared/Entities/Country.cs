0209905902f3dac94eee21d2e27334cf0754ccdd
francisco Manuel Rincon Vargas <fmrinconv@gmail.com>
2026-09-05 18:34:25 -0600

    Se crea primer control de Paises ya con funcionalidad GET y POST

--- ORDERS/Orders.Shared/Entities/Country.cs
+++ ORDERS/Orders.Shared/Entities/Country.cs
@@ -8,6 +8,6 @@ public class Country
 
     [Display(Name = "Pais")]
     [MaxLength(100, ErrorMessage = "{0} no puede exceder los {1} caracteres.")]
-    [Required(ErrorMessage = "El nombre de {0} es requerido.")]
+    [Required(ErrorMessage = "El nombre del campo: \"{0}\" es obligatorio.")]
     public string Name { get; set; } = null!;
 }
